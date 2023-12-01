c28d51d576da0d28b6ad7428a827503f8c424a58
eduardop1p <el702985@gmail.com>
2023-12-01 09:28:16 -0300

    correções de bugs na  nav

--- next.config.js
+++ next.config.js
@@ -16,6 +16,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
